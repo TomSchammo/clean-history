f8c61dcbbc7f436140453810c626c549f094f909
Tom Schammo <tom@tomschammo.com>
2022-03-29 20:34:19 +0200

    feat(histfile): Add function to figure out histfile path
    Add a function to figure out the path of the file that contains the
    shell history.
    
    Currently only looking for the history of the z-shell.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,34 @@ fn main() {
 }
 
 #[cfg(unix)]
+use std::env;
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let hist_file = get_histfile_path();
+    println!("{}", hist_file.to_str().unwrap());
+}
+
+fn get_histfile_path() -> PathBuf {
+    let mut hist_file = PathBuf::new();
+
+    match env::var("XDG_CONFIG_HOME") {
+        Ok(xdg_config_home_value) => hist_file.push(xdg_config_home_value),
+        Err(_) => {
+            eprintln!("No XDG_CONFIG_HOME environment variable set, falling back to $HOME/.config");
+
+            match env::var("HOME") {
+                Ok(home_value) => hist_file.push(home_value),
+                Err(e) => {
+                    eprintln!("No HOME environment variable set, aborting...");
+                    panic!("{}", e);
+                }
+            }
+            hist_file.push(".config");
+        }
+    };
+
+    hist_file.push(Path::new("zsh/histfile"));
+
+    hist_file
 }
