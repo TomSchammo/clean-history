0e51fe6fe5444ed4711a4b08155cac9df391e540
Tom Schammo <tom@tomschammo.com>
2022-03-30 01:52:58 +0200

    test(get_histfile_path): Add tests for the `get_histfile_path` function
    Add tests for the `get_histfile_path` function.
    
    NOTE: The tests individually run and pass, but because environment
    variables are a shared resource and the tests run concurrently in the
    same process, they get in eachothers way.
    
    Need to fix that...

--- src/main.rs
+++ src/main.rs
@@ -35,3 +35,99 @@ fn get_histfile_path() -> PathBuf {
 
     hist_file
 }
+
+#[cfg(test)]
+mod tests {
+    // FIXME tests don't work yet, because environment variables are a shared resource and the
+    // tests run concurrently. So they end up getting in the way of eachother.
+
+    use super::*;
+
+    struct Environment {
+        home: String,
+        xdg_config_home: String,
+    }
+
+    impl Drop for Environment {
+        fn drop(&mut self) {
+            let h = self.home.clone();
+            let x = self.xdg_config_home.clone();
+
+            env::set_var("HOME", h);
+            env::set_var("XDG_CONFIG_HOME", x);
+        }
+    }
+
+    impl Environment {
+        fn new() -> Self {
+            let home = env::var("HOME");
+            let xdg_config_home = env::var("XDG_CONFIG_HOME");
+
+            if home.is_err() || xdg_config_home.is_err() {
+                if let Err(e) = home {
+                    println!("{}", e)
+                };
+
+                if let Err(e) = xdg_config_home {
+                    println!("{}", e)
+                };
+                panic!("Cannot retrieve environment variables");
+            }
+
+            Environment {
+                home: home.unwrap(),
+                xdg_config_home: xdg_config_home.unwrap(),
+            }
+        }
+
+        fn prepare(home: Option<&str>, xdg_config_home: Option<&str>) {
+            env::remove_var("HOME");
+            env::remove_var("XDG_CONFIG_HOME");
+
+            if let Some(home) = home {
+                env::set_var("HOME", home);
+            }
+
+            if let Some(xdg_config_home) = xdg_config_home {
+                env::set_var("XDG_CONFIG_HOME", xdg_config_home);
+            }
+        }
+    }
+
+    impl Default for Environment {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    #[test]
+    fn test_get_histfile_path_xdg_config_home_set() {
+        let _ = Environment::new();
+        tests::Environment::prepare(Some("/home/"), Some("/home/user/config"));
+
+        assert_eq!(
+            get_histfile_path().to_str().unwrap(),
+            "/home/user/config/zsh/histfile"
+        );
+    }
+
+    #[test]
+    fn test_get_histfile_path_home_set() {
+        let _ = Environment::new();
+        tests::Environment::prepare(Some("/home/user"), None);
+
+        assert_eq!(
+            get_histfile_path().to_str().unwrap(),
+            "/home/user/.config/zsh/histfile"
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_histfile_path_nothing_set() {
+        let _ = Environment::new();
+        tests::Environment::prepare(None, None);
+
+        let _ = get_histfile_path();
+    }
+}
