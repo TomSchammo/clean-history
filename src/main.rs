8c8bee93e66f233917d4b8757a4a29f6b5dc584e
Tom Schammo <tom@tomschammo.com>
2022-03-30 03:48:04 +0200

    test(get_histfile_path): Fix tests and improve error handling
    The tests now run all together, when they are ran using only one thread.
    This can accomplished by running them using
    `cargo test -- -test-threads=1`.
    
    Also improve error handling by not panicking every time a variable
    could not be read. It is entirely plausible that one or more environment
    variables are not present and therefore cannot be 'restored' after.
    
    So if an environment variable cannot be read because it does not exist,
    it is simply ignored.

--- src/main.rs
+++ src/main.rs
@@ -46,54 +46,64 @@ fn get_histfile_path() -> PathBuf {
 
 #[cfg(test)]
 mod tests {
-    // FIXME tests don't work yet, because environment variables are a shared resource and the
-    // tests run concurrently. So they end up getting in the way of eachother.
 
     use super::*;
+    use std::env::VarError;
 
     struct Environment {
-        home: String,
-        xdg_config_home: String,
-        hist_file_path: String,
+        home: Option<String>,
+        xdg_config_home: Option<String>,
+        hist_file_path: Option<String>,
     }
 
     impl Drop for Environment {
         fn drop(&mut self) {
-            let h = self.home.clone();
-            let x = self.xdg_config_home.clone();
-            let i = self.hist_file_path.clone();
+            if let Some(value) = self.home.clone() {
+                env::set_var("HOME", value);
+            }
 
-            env::set_var("HOME", h);
-            env::set_var("XDG_CONFIG_HOME", x);
-            env::set_var("HISTFILE", i);
+            if let Some(value) = self.xdg_config_home.clone() {
+                env::set_var("XDG_CONFIG_HOME", value);
+            }
+
+            if let Some(value) = self.hist_file_path.clone() {
+                env::set_var("HISTFILE", value);
+            }
         }
     }
 
     impl Environment {
         fn new() -> Self {
-            let home = env::var("HOME");
-            let xdg_config_home = env::var("XDG_CONFIG_HOME");
+            let home_path = env::var("HOME");
+            let xdg_config_home_path = env::var("XDG_CONFIG_HOME");
             let hist_file_path = env::var("HISTFILE");
 
-            if home.is_err() || xdg_config_home.is_err() || hist_file_path.is_err() {
-                if let Err(e) = home {
-                    eprintln!("HOME: {}", e)
-                };
+            let mut home = None;
+            let mut xdg_config_home = None;
+            let mut hist_file = None;
 
-                if let Err(e) = xdg_config_home {
-                    eprintln!("XDG_CONFIG_HOME: {}", e)
-                };
+            if let Ok(home_path) = home_path {
+                home = Some(home_path);
+            } else if home_path.err().unwrap() != VarError::NotPresent {
+                panic!("Error when retrieving environment variable 'HOME'");
+            }
 
-                if let Err(e) = hist_file_path {
-                    eprintln!("HISTFILE: {}", e)
-                };
-                panic!("Cannot retrieve environment variables");
+            if let Ok(xdg_config_home_path) = xdg_config_home_path {
+                xdg_config_home = Some(xdg_config_home_path);
+            } else if xdg_config_home_path.err().unwrap() != VarError::NotPresent {
+                panic!("Error when retrieving environment variable 'XDG_CONFIG_HOME'");
+            }
+
+            if let Ok(hist_file_path) = hist_file_path {
+                hist_file = Some(hist_file_path);
+            } else if hist_file_path.err().unwrap() != VarError::NotPresent {
+                panic!("Error when retrieving environment variable 'HISTFILE'");
             }
 
             Environment {
-                home: home.unwrap(),
-                xdg_config_home: xdg_config_home.unwrap(),
-                hist_file_path: hist_file_path.unwrap(),
+                home,
+                xdg_config_home,
+                hist_file_path: hist_file,
             }
         }
 
@@ -102,9 +112,9 @@ mod tests {
             xdg_config_home: Option<&str>,
             hist_file_path: Option<&str>,
         ) {
-            // env::remove_var("HOME");
-            // env::remove_var("XDG_CONFIG_HOME");
-            // env::remove_var("HISTFILE");
+            env::remove_var("HOME");
+            env::remove_var("XDG_CONFIG_HOME");
+            env::remove_var("HISTFILE");
 
             if let Some(home) = home {
                 env::set_var("HOME", home);
