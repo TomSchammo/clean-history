3f2aa18369bbd57a061cc50230971637ad8be31f
Tom Schammo <tom@tomschammo.com>
2022-04-10 10:44:44 +0200

    feat: Add commandline arguments
    The program accepts the following (optional) commandline arguments:
        - deamonize: Will start the program as a deamon. Is not implemented
          yet.
        - history: A path to a history file. If this argument is provided,
          the program will use that histfile, else it fill figure out which
          histfile to use like before.
    
    Additional dependencies: clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use self::file_operations::filter;
+use clap::Parser;
+use std::path::Path;
 
 #[cfg(not(unix))]
 fn main() {
@@ -9,7 +11,33 @@ fn main() {
 mod file_operations;
 mod setup;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+    /// Launch progress as a deamon
+    #[clap(short, long, takes_value = false, help = "Launch program as a deamon")]
+    deamonize: bool,
+
+    #[clap(
+        short,
+        long,
+        takes_value = true,
+        help = "Provide the path to a history file manually"
+    )]
+    history: Option<String>,
+}
+
 fn main() {
-    let hist_file = setup::get_histfile_path();
-    filter(hist_file);
+    let args = Args::parse();
+
+    if args.deamonize {
+        unimplemented!("Start program as a deamon");
+    } else {
+        let hist_file = match args.history {
+            Some(arg) => Path::new(&arg).to_path_buf(),
+            None => setup::get_histfile_path(),
+        };
+
+        filter(hist_file);
+    }
 }
