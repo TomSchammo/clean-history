c1e1cd09153c5f18c16fc87b3494659805cc7499
Tom Schammo <tom@tomschammo.com>
2022-08-24 13:13:25 +0200

    feat(config): Add support for config files
    Configuration will be possible by using a config file (`config.json`)
    stored in `$XDG_CONFIG_HOME/clean-history/`

--- src/setup.rs
+++ src/setup.rs
@@ -1,5 +1,5 @@
-use std::env;
 use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 pub fn get_histfile_path() -> PathBuf {
     let mut hist_file = PathBuf::new();
@@ -31,6 +31,134 @@ pub fn get_histfile_path() -> PathBuf {
     }
 
     hist_file
+}
+
+/// Looks for the config in $XDG_CONFIG_HOME/clean-history/config.json.
+/// If $XDG_CONFIG_HOME is not set, it defaults to $HOME/.config.
+/// If $HOME is not set function aborts.
+fn get_config_path() -> Option<PathBuf> {
+    let mut config_file = PathBuf::new();
+
+    match env::var("XDG_CONFIG_HOME") {
+        Ok(xdg_config_home_value) => config_file.push(xdg_config_home_value),
+        Err(_) => {
+            eprintln!("No XDG_CONFIG_HOME environment variable set, falling back to $HOME/.config");
+
+            match env::var("HOME") {
+                Ok(home_value) => config_file.push(home_value),
+                Err(e) => {
+                    eprintln!("No HOME environment variable set, aborting...");
+                    eprintln!("{e}");
+                    return None;
+                }
+            }
+            config_file.push(".config");
+        }
+    }
+
+    config_file.push("clean-history/config.json");
+
+    Some(config_file)
+}
+
+/// Reads the data in config file.
+/// The read data will then be parsed, wrapped in a
+/// `Config` and returned.
+fn get_config(config_path: PathBuf) -> Option<Config> {
+    let file_contents = fs::read_to_string(config_path).ok();
+
+    if let Some(file_contents) = file_contents {
+        return match json::parse(&file_contents) {
+            Ok(val) => Some(parse_json_object(val)),
+            Err(_) => None,
+        };
+    }
+    None
+}
+
+/// Parses the json value returned by `json::parse`.
+/// This function will look for the memebers of `Config`.
+///
+/// If a member is not found or defined incorrectly,
+/// it will simply be set to `None`.
+fn parse_json_object(object: json::JsonValue) -> Config {
+    let histfile: Option<PathBuf> = {
+        if object["histfile"].is_null() {
+            None
+        } else {
+            object["histfile"].as_str().map(PathBuf::from)
+        }
+    };
+
+    let blacklist: Option<Vec<String>> = {
+        if object["blacklist"].is_null() {
+            // 'blacklist' is not set in config
+            None
+        } else if object["blacklist"].is_array() {
+            let vals = object["blacklist"]
+                .members()
+                .filter(|val| val.as_str().is_some())
+                .map(|val| val.to_string())
+                .collect();
+            Some(vals)
+        } else {
+            // 'blacklist' is not an array, so set incorrectly
+            None
+        }
+    };
+
+    let max_char_limit: Option<u64> = {
+        if object["max_char_limit"].is_null() {
+            None
+        } else {
+            // 'max_char_limit' is either returned, or not set correctly
+            object["max_char_limit"].as_u64()
+        }
+    };
+
+    let min_char_limit: Option<u64> = {
+        if object["min_char_limit"].is_null() {
+            None
+        } else {
+            // 'min_char_limit' is either returned, or not set correctly
+            object["min_char_limit"].as_u64()
+        }
+    };
+
+    Config {
+        histfile,
+        blacklist,
+        max_char_limit,
+        min_char_limit,
+    }
+}
+
+pub struct Config {
+    /// Location of the histfile.
+    /// This value is used if provided.
+    histfile: Option<PathBuf>,
+
+    /// Commands that will automatically be removed from the history
+    blacklist: Option<Vec<String>>,
+
+    /// Commands longer than 'max_char_limit' will automatically be removed from the history
+    max_char_limit: Option<u64>,
+
+    /// Commands shorter than 'min_char_limit' will automatically be removed from the history
+    min_char_limit: Option<u64>,
+}
+
+/// Reads the config file and parses it into a `Config`.
+pub fn config() -> Config {
+    if let Some(path) = get_config_path() {
+        if let Some(config) = get_config(path) {
+            config
+        } else {
+            todo!("What to do when config could not be retrieved?")
+        }
+    } else {
+        todo!("What to do when config could not be found?")
+    }
 }
 
 #[cfg(test)]
